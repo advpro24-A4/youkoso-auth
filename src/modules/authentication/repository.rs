5b682fc5a4318513440192613b5c3583c45cb51e
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-04-25 20:50:55 +0700

    Create authentication repository skeleton

--- src/modules/authentication/repository.rs
+++ src/modules/authentication/repository.rs
@@ -1,5 +1,23 @@
-trait AuthenticationRepository {}
+use crate::models::user::model::user::User;
 
-pub struct AuthenticationRepositoryImpl {}
+trait AuthenticationRepositoryTrait {
+    fn create_user(user: User) -> User;
+    fn find_by_email(email: String) -> User;
+    fn find_by_id(id: String) -> User;
+}
 
-impl AuthenticationRepository for AuthenticationRepositoryImpl {}
+pub struct AuthenticationRepository {}
+
+impl AuthenticationRepositoryTrait for AuthenticationRepository {
+    fn find_by_id(id: String) -> User {
+        unimplemented!();
+    }
+
+    fn create_user(user: User) -> User {
+        unimplemented!();
+    }
+
+    fn find_by_email(email: String) -> User {
+        unimplemented!();
+    }
+}
