ecbb68c3b36a01d4ddc8cb93d02cfb712ead3e50
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-04-25 20:51:01 +0700

    Create service skeleton

--- src/modules/authentication/service.rs
+++ src/modules/authentication/service.rs
@@ -1,5 +1,23 @@
-trait AuthenticationService {}
+use crate::models::user::model::user::User;
+
+trait AuthenticationService {
+    fn login(user: User) -> User;
+    fn register(user: User) -> User;
+    fn verify_password(password: String) -> bool;
+}
 
 pub struct AuthenticationServiceImpl {}
 
-impl AuthenticationService for AuthenticationServiceImpl {}
+impl AuthenticationService for AuthenticationServiceImpl {
+    fn login(user: User) -> User {
+        unimplemented!()
+    }
+
+    fn register(user: User) -> User {
+        unimplemented!()
+    }
+
+    fn verify_password(password: String) -> bool {
+        unimplemented!()
+    }
+}
