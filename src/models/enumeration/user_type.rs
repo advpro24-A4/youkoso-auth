4b4dd93237d5893b194b295be9be687402485a99
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-04-25 12:29:06 +0700

    Edit user model

--- src/models/enumeration/user_type.rs
+++ src/models/enumeration/user_type.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+#[derive(Debug, Clone)]
 pub enum UserRole {
     Admin,
     Customer,
--- src/models/user.rs
+++ src/models/user.rs
@@ -1,9 +1,10 @@
 use bcrypt::{hash_with_result, Version, DEFAULT_COST};
-use diesel::sql_types::Date;
+use chrono::NaiveDate;
 use nanoid::nanoid;
 
 use crate::models::enumeration::user_type::UserRole;
 
+#[derive(Debug, Clone)]
 pub struct User {
     id: String,
     email: String,
@@ -12,17 +13,31 @@ pub struct User {
     password: String,
     role: UserRole,
     address: Option<String>,
-    birth_date: Option<Date>,
+    birth_date: Option<NaiveDate>,
     phone_number: Option<String>,
 }
 
-trait NewTrait {
-    fn new(&self, email: String, password: String, role: UserRole) -> User;
+pub trait UserTrait {
+    fn new(email: String, password: String, role: UserRole) -> User;
+    fn get_id(&self) -> String;
+    fn get_email(&self) -> String;
+    fn get_username(&self) -> String;
+    fn get_name(&self) -> String;
+    fn get_password(&self) -> String;
+    fn get_role(&self) -> String;
+    fn get_address(&self) -> String;
+    fn get_birth_date(&self) -> NaiveDate;
+    fn get_phone_number(&self) -> String;
+    fn set_name(&self, name: String);
+    fn set_username(&self, username: String);
+    fn set_address(&self, address: String);
+    fn set_birth_date(&self, date: NaiveDate);
+    fn set_phone_number(&self, phone_number: String);
     fn encrypt_password(&self) -> String;
 }
 
-impl NewTrait for User {
-    fn new(&self, email: String, password: String, role: UserRole) -> User {
+impl UserTrait for User {
+    fn new(email: String, password: String, role: UserRole) -> User {
         User {
             id: nanoid!(),
             email,
@@ -34,6 +49,62 @@ impl NewTrait for User {
             birth_date: None,
             phone_number: None,
         }
+    }
+
+    fn get_id(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_email(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_username(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_name(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_password(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_role(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_address(&self) -> String {
+        unimplemented!()
+    }
+
+    fn get_birth_date(&self) -> NaiveDate {
+        unimplemented!()
+    }
+
+    fn get_phone_number(&self) -> String {
+        unimplemented!()
+    }
+
+    fn set_name(&self, name: String) {
+        unimplemented!()
+    }
+
+    fn set_username(&self, username: String) {
+        unimplemented!()
+    }
+
+    fn set_address(&self, address: String) {
+        unimplemented!()
+    }
+
+    fn set_birth_date(&self, date: NaiveDate) {
+        unimplemented!()
+    }
+
+    fn set_phone_number(&self, phone_number: String) {
+        unimplemented!()
     }
 
     fn encrypt_password(&self) -> String {
