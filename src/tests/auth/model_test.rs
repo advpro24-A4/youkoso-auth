3c9be6dbde6e512e5f57784435a798a8943df0d9
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-04-25 18:26:25 +0700

    [RED] Refactor user and create new test for model

--- src/tests/auth/model_test.rs
+++ src/tests/auth/model_test.rs
@@ -1,80 +1,85 @@
-use crate::models::enumeration::user_type::UserRole;
-use crate::models::user::{User, UserTrait};
-
-fn init_user() -> User {
-    User::new(
-        "hkalipaksi@kaka.com".to_owned(),
-        "sukses".to_owned(),
-        UserRole::Customer,
-    )
-}
-
-#[test]
-fn test_general_user_getter() {
-    let user: User = init_user();
-    let cloned_user: User = user.clone();
-
-    _ = user.set_phone_number("0812131380037".to_owned());
-    assert_eq!(user.get_id(), cloned_user.get_id());
-    assert_eq!(user.get_email(), "hkalipaksi@kaka.com");
-    assert_eq!(user.get_password(), "sukses");
-}
+use crate::models::{
+    enumeration::user_type::UserRole,
+    user::{
+        builder::user_builder::{UserBuilder, UserBuilderTrait},
+        director::{UserDirector, UserDirectorTrait},
+        model::{
+            profile::{Profile, ProfileTrait},
+            user::{User, UserTrait},
+        },
+    },
+};
 
 #[test]
-fn test_set_get_name() {
-    let user: User = init_user();
-    _ = user.set_name("Haekal".to_owned());
-    assert_eq!(user.get_name(), "Haekal");
-}
-
-#[test]
-fn test_set_get_address() {
-    let user: User = init_user();
-    _ = user.set_address("Jl. Jambu X No.19".to_owned());
-    assert_eq!(user.get_address(), "Jl. Jambu X No.19")
-}
-
-#[test]
-fn test_set_get_birth_date() {
-    let user: User = init_user();
-    _ = user.set_birth_date(chrono::NaiveDate::from_ymd_opt(2024, 12, 24).unwrap());
-    assert_eq!(
-        user.get_birth_date(),
-        chrono::NaiveDate::from_ymd_opt(2024, 12, 24).unwrap()
+fn test_create_register_customer() {
+    let mut builder = UserBuilder::default();
+    _ = UserDirector::construct_register_customer_user(
+        &mut builder,
+        "hkalipaksi@outlook.com".to_owned(),
+        "Xpaksi19".to_owned(),
     );
-}
-
-#[test]
-fn test_set_get_username() {
-    let user: User = init_user();
-    _ = user.set_username("mhmdhaekal".to_owned());
-    assert_eq!(user.get_phone_number(), "081213180037".to_owned());
-}
-
-#[test]
-fn test_create_customer_role() {
-    let customer_user = User::new(
-        "hkalipaksi@kaka.com".to_owned(),
-        "sukses".to_owned(),
-        UserRole::Customer,
-    );
+    let user_customer: User = builder.build();
 
     assert_eq!(
-        customer_user.get_role().to_string(),
+        user_customer.role().to_string(),
         UserRole::Customer.to_string()
-    )
+    );
+    assert_eq!(user_customer.profile().is_none(), true);
 }
 
 #[test]
-fn test_create_admin_role() {
-    let admin_user = User::new(
-        "hkalipaksi@kaka.com".to_owned(),
-        "sukses".to_owned(),
-        UserRole::Admin,
+fn test_create_admin() {
+    let mut builder = UserBuilder::default();
+    _ = UserDirector::construct_admin_user(
+        &mut builder,
+        "hkalipaksi@outlook.com".to_owned(),
+        "Xpaksi19".to_owned(),
+    );
+    let user_admin: User = builder.build();
+
+    assert_eq!(user_admin.role().to_string(), UserRole::Admin.to_string());
+    assert_eq!(user_admin.profile().is_none(), true);
+}
+
+#[test]
+fn test_create_profile_customer() {
+    let mut builder = UserBuilder::default();
+    _ = UserDirector::construct_register_customer_user(
+        &mut builder,
+        "hkalipaksi@outlook.com".to_owned(),
+        "Xpaksi19".to_owned(),
+    );
+    let registered_customer: User = builder.build();
+
+    let profile = Profile::new(
+        "Haekal".to_string(),
+        "hkalipaksi".to_string(),
+        "Jl. Jambu X No.19".to_string(),
+        chrono::NaiveDate::from_ymd_opt(2004, 04, 24).unwrap(),
+        "081213180037".to_string(),
     );
 
+    let mut builder = UserBuilder::default();
+    _ = UserDirector::construct_customer_user(&mut builder, registered_customer, profile);
+    let customer_user: User = builder.build();
+
     assert_eq!(
-        admin_user.get_role().to_string(),
-        UserRole::Admin.to_string()
-    )
+        customer_user.role().to_string(),
+        UserRole::Customer.to_string()
+    );
+
+    assert_eq!(customer_user.profile().is_none(), false);
+
+    let user_profile = customer_user.profile().as_ref().unwrap();
+
+    assert_eq!(user_profile.name(), "Haekal");
+    assert_eq!(user_profile.username(), "hkalipaksi");
+    assert_eq!(user_profile.address(), "Jl. Jambu X No.19");
+    assert_eq!(
+        user_profile.birth_date().to_string(),
+        chrono::NaiveDate::from_ymd_opt(2004, 04, 24)
+            .unwrap()
+            .to_string()
+    );
+    assert_eq!(user_profile.phone_number(), "081213180037")
 }
